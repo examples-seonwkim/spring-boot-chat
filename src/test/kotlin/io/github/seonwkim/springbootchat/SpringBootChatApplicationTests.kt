7cfb3f60a3cd5194f8fe9e7e22e569979b9c5587
김선우 <seonwoo960000@toss.im>
2025-05-27 20:34:01 +0900

    Initialize

---
+++ src/test/kotlin/io/github/seonwkim/springbootchat/SpringBootChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.seonwkim.springbootchat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
